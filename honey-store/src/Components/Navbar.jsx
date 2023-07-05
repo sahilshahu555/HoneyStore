e75b59fcef0efea382cb55e3a869af5e4fbc59eb
Sahil Shahu <sahilshahu555@gmail.com>
2023-07-05 17:48:22 +0530

    edited

--- honey-store/src/Components/Navbar.jsx
+++ honey-store/src/Components/Navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
-    <div id='navbar'>
+    <div id='navbar' style={{display:"flex",justifyContent:"space-around",
+     alignItems:"center",
+     gap:"10px"
+    }}>
       <Link to={"/"}>Home</Link>
       <Link to={"/product"}>Product Page</Link>
       <Link to={"/login"}>Login</Link>
